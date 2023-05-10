22f85a31f79e3ff8f12c5fb792077e334c1bf866
SantyVargasIllanes <vis0033513@est.univalle.edu>
2023-05-10 14:16:38 -0400

    mi primer hola mundo

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GIT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, suma;
+            Console.WriteLine("Ingrese el primer numero: ");
+            num1 =int.Parse(Console.ReadLine());
+            Console.WriteLine("ingrese el segundo numero: ");
+            num2=int.Parse(Console.ReadLine());
+
+            suma = num1 + num2;
+            Console.WriteLine("la suma es ", suma);
+        }
+    }
+}
