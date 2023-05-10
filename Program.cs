8da279cba10c5a6181e22ab4fa14b3b329a80ff4
SantyVargasIllanes <vis0033513@est.univalle.edu>
2023-05-10 14:24:08 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -5,13 +5,16 @@
         static void Main(string[] args)
         {
             int num1, num2, suma;
+            string nombre;
+            Console.WriteLine("Ingresa tu nombre");
+            nombre= Console.ReadLine();
             Console.WriteLine("Ingrese el primer numero: ");
             num1 =int.Parse(Console.ReadLine());
             Console.WriteLine("ingrese el segundo numero: ");
             num2=int.Parse(Console.ReadLine());
 
             suma = num1 + num2;
-            Console.WriteLine("la suma es ", suma);
+            Console.WriteLine(nombre +" la suma es "+ suma);
         }
     }
 }
